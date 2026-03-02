88298f5de4b85de410c8410f01ebc220ab27e3f7
unmeiongaku <nguyenhoangminhdo@gmail.com>
2026-03-02 19:23:54 +0700

    add recipe-kernel-ll-major-minor

--- recipes-kernel/ll-major-minor-mod/files/ll-major-minor.c
+++ recipes-kernel/ll-major-minor-mod/files/ll-major-minor.c
@@ -21,34 +21,108 @@
 #include <linux/kernel.h>
 #include <linux/fs.h>       /* Define alloc_chrdev_region(), register_chrdev_region() */
 #include <linux/init.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
 
 #define DRIVER_AUTHOR "desmtiny nguyenhoangminhdo@gmail.com"
 #define DRIVER_DESC   "Hello world kernel module"
 #define DRIVER_VERS   "1.0"
+#define DEVICE_NAME "m_cdev"
 
-struct m_foo_dev {
+static struct m_foo_dev {
     dev_t dev_num;
+    struct cdev m_cdev;
+    struct class *m_class;
+    struct device *m_device;
 } mdev;
 
+/* ================= FILE OPERATIONS ================= */
+static int m_open(struct inode *inode, struct file *file)
+{
+    pr_info("Device opened\n");
+    return 0;
+}
+
+static int m_release(struct inode *inode, struct file *file)
+{
+    pr_info("Device closed\n");
+    return 0;
+}
+
+static ssize_t m_read(struct file *file, char __user *buf,
+                      size_t len, loff_t *offset)
+{
+    char msg[] = "Hello from kernel space!\n";
+    size_t msg_len = sizeof(msg);
+
+    if (*offset >= msg_len)
+        return 0;
+
+    if (copy_to_user(buf, msg, msg_len))
+        return -EFAULT;
+
+    *offset += msg_len;
+    return msg_len;
+}
+
+static const struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open  = m_open,
+    .release = m_release,
+    .read  = m_read,
+};
 
 static int __init chdev_init(void)
 {
+    int ret;
     /* 1.0 Dynamic allocating device number (cat /proc/devices) */
-    if (alloc_chrdev_region(&mdev.dev_num, 0, 1, "m_cdev") < 0) {
+    ret = alloc_chrdev_region(&mdev.dev_num, 0, 1, DEVICE_NAME);
+    if (ret < 0) {
 	    pr_err("Failed to alloc chrdev region\n");
-	    return -1;
+	    return ret;
     }
     // dev_t dev = MKDEV(173, 0);
     // register_chrdev_region(&mdev.dev_num, 1, "m-cdev")
     pr_info("Major = %d Minor = %d\n", MAJOR(mdev.dev_num), MINOR(mdev.dev_num));
-    pr_info("Hello World!\n");
+    /* 2. Init cdev */
+    cdev_init(&mdev.m_cdev, &fops);
+    mdev.m_cdev.owner = THIS_MODULE;
+    /* 3. Add cdev to kernel */
+    ret = cdev_add(&mdev.m_cdev,mdev.dev_num, 1);
+    if (ret < 0) {
+        pr_err("Failed to add cdev\n");
+        unregister_chrdev_region(mdev.dev_num, 1);
+        return ret;
+    }
+    /* 4. Create class */
+    mdev.m_class = class_create(DEVICE_NAME);
+    if (IS_ERR(mdev.m_class)) {
+        pr_err("Failed to create class\n");
+        cdev_del(&mdev.m_cdev);
+        unregister_chrdev_region(mdev.dev_num, 1);
+        return PTR_ERR(mdev.m_class);
+    }
+    /* 5. Create device node in /dev */
+    mdev.m_device = device_create(mdev.m_class, NULL, mdev.dev_num, NULL, DEVICE_NAME);
+        if (IS_ERR(mdev.m_device)) {
+        pr_err("Failed to create device\n");
+        class_destroy(mdev.m_class);
+        cdev_del(&mdev.m_cdev);
+        unregister_chrdev_region(mdev.dev_num, 1);
+        return PTR_ERR(mdev.m_device);
+    }
+    pr_info("Driver loaded successfully\n");
     return 0;
 }
 
 static void __exit chdev_exit(void)
 {
+    device_destroy(mdev.m_class, mdev.dev_num);
+    class_destroy(mdev.m_class);
+    cdev_del(&mdev.m_cdev);
     unregister_chrdev_region(mdev.dev_num, 1);              /* 1.0 */
-    pr_info("Goodbye!\n");
+    pr_info("Driver unloaded\n\n");
 }
 
 module_init(chdev_init);
