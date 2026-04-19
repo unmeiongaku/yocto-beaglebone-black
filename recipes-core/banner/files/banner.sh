8e00307e338f3f19ca9a971b654b87c92d5df841
unmeiongaku <nguyenhoangminhdo@gmail.com>
2026-04-19 15:13:18 +0700

    add banner.sh Qt

--- recipes-core/banner/files/banner.sh
+++ recipes-core/banner/files/banner.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# ===== COLOR =====
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+CYAN="\033[1;36m"
+WHITE="\033[1;37m"
+NC="\033[0m"
+
+BANNERCOLOUR=$BLUE
+
+# ===== SENSOR =====
 TEMP_RAW=$(cat /sys/bus/iio/devices/iio:device1/in_temp_raw)
 TEMP_SCALE=$(cat /sys/bus/iio/devices/iio:device1/in_temp_scale)
 
@@ -13,21 +26,31 @@ SEA=1013.25
 
 ALT=$(awk "BEGIN {printf \"%.2f\", 44330 * (1.0 - ($PRESS_HPA / $SEA)^0.1903)}")
 
+# ===== TEMP COLOR =====
+if awk "BEGIN {exit !($TEMP > 60)}"; then
+    TEMP_COLOR=$RED
+elif awk "BEGIN {exit !($TEMP > 40)}"; then
+    TEMP_COLOR=$YELLOW
+else
+    TEMP_COLOR=$GREEN
+fi
 
-echo "
-  ____  _____ ____  __  __ _____ ___ _   ___   __
- |  _ \| ____/ ___||  \/  |_   _|_ _| \ | \ \ / /
- | | | |  _| \___ \| |\/| | | |  | ||  \| |\ V / 
- | |_| | |___ ___) | |  | | | |  | || |\  | | |  
- |____/|_____|____/|_|  |_| |_| |___|_| \_| |_|  
-                                                  
-Uptime       : $(uptime)
-Hostname     : $(hostname)
-Disk Usage   : $(df -h | awk '/\/$/ {print $3 " used of " $2}')
-Memory       : $(free | awk '/Mem:/ {printf "%.1fM used of %.1fM", $3/1024, $2/1024}')
-Time         : $(cat /sys/bus/i2c/devices/2-0068/time)
-
-Temperature  : ${TEMP} °C
-Pressure     : ${PRESS} Pa
-Altitute     : ${ALT} m
+# ===== OUTPUT =====
+echo -e "
+${CYAN}╔══════════════════════════════════════════════════╗
+║${NC} ${BANNERCOLOUR} ____  _____ ____  __  __ _____ ___ _   ___   __${NC} ${CYAN}║
+║${NC} ${BANNERCOLOUR}|  _ \\| ____/ ___||  \\/  |_   _|_ _| \\ | \\ \\ / /${NC} ${CYAN}║
+║${NC} ${BANNERCOLOUR}| | | |  _| \\___ \\| |\\/| | | |  | ||  \\| |\\ V / ${NC} ${CYAN}║
+║${NC} ${BANNERCOLOUR}| |_| | |___ ___) | |  | | | |  | || |\\  | | | ${NC} ${CYAN} ║
+║${NC} ${BANNERCOLOUR}|____/|_____|____/|_|  |_| |_| |___|_| \\_| |_|${NC} ${CYAN}  ║
+║                                              	   ║
+║ ${WHITE}Hostname  ${NC}: $(hostname)                     	   ║
+║ ${WHITE}Uptime    ${NC}: $(uptime -p)                  	   ║
+║ ${WHITE}Time      ${NC}: $(cat /sys/bus/i2c/devices/2-0068/time) 		   ║
+║                                              	   ║
+║ ${WHITE}Disk      ${NC}: $(df -h | awk '/\/$/ {print $3 " / " $2}')               	   ║
+║ ${WHITE}Memory    ${NC}: $(free | awk '/Mem:/ {printf "%.1fM / %.1fM", $3/1024, $2/1024}') 			   ║
+║                                                  ║
+║ ${WHITE}Sensors   ${NC}: ${TEMP_COLOR}${TEMP}°C${NC} | ${BLUE}${PRESS} Pa${NC} | ${GREEN}${ALT} m${NC}      ║
+${CYAN}╚══════════════════════════════════════════════════╝${NC}
 "
